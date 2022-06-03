b013dc9ea7f1837f420190f6bed269420f2ef4d8
Taruntej Kanakamalla <taruntejk@live.com>
2022-06-03 12:29:16 +0530

    Updated the copyrights in the header

--- videoeffects/src/gstneovideoconv.h
+++ videoeffects/src/gstneovideoconv.h
@@ -1,5 +1,5 @@
 /* GStreamer
- * Copyright (C) 2022 FIXME <fixme@example.com>
+ * Copyright (C) 2022 Taruntej Kanakamalla <taruntejk@live.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
